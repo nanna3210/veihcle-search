b3ea3470055c60bfe4bdd458d9aa0bead82da614
nanna <navkishoredas@gmail.com>
2024-03-26 16:14:18 +0530

    project intial setup repo

---
+++ src/main/java/com/learn2code/veihcle/api/search/VeihcleSearchApplication.java
@@ -0,0 +1,20 @@
+package com.learn2code.veihcle.api.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeihcleSearchApplication {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(VeihcleSearchApplication.class, args);
+
+
+
+
+	}
+
+}
---
+++ src/test/java/com/learn2code/veihcle/api/search/VeihcleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.veihcle.api.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeihcleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
