0aeca3903a0c7ba70dad84ead662475eec918da3
nanna <navkishoredas@gmail.com>
2024-03-27 20:11:00 +0530

    created ManufatureDao.java and ManufatureDao classes

---
+++ src/main/java/com/learn2code/veihcle/api/search/dao/ManufactureYear.java
@@ -0,0 +1,8 @@
+package com.learn2code.veihcle.api.search.dao;
+
+import com.learn2code.veihcle.api.search.entity.ManufacturYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManufactureYear extends JpaRepository<ManufacturYear, Integer> {
+
+}
---
+++ src/main/java/com/learn2code/veihcle/api/search/dao/ManufatureDao.java
@@ -0,0 +1,11 @@
+package com.learn2code.veihcle.api.search.dao;
+
+import com.learn2code.veihcle.api.search.entity.Manufacturer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManufatureDao  extends JpaRepository<Manufacturer, Integer> {
+
+
+
+
+}
